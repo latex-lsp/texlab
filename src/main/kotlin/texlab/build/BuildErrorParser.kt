6b52898b252f5e02674a4c928276f3d1036ad76b
Patrick Förster <pfoerster@users.noreply.github.com>
2018-12-28 14:20:09 +0100

    Fix build log parser on JDK10

--- src/main/kotlin/texlab/build/BuildErrorParser.kt
+++ src/main/kotlin/texlab/build/BuildErrorParser.kt
@@ -3,15 +3,17 @@ package texlab.build
 import java.io.File
 import java.net.URI
 import java.nio.file.Paths
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 object BuildErrorParser {
     private const val MAX_LINE_LENGTH = 79
 
     private val PACKAGE_MESSAGE_REGEX = """^\([a-zA-Z_\-]+\)\s*(?<Message>.*)$""".toRegex()
     private val FILE_REGEX = """\((?<File>[^\r\n()]+\.(tex|sty|cls))""".toRegex()
-    private val TEX_ERROR_REGEX = """^! ((?<Message1>(.|\r|\n)*?)\r?\nl\.(?<Line>\d+)|(?<Message2>[^\r\n]*))""".toRegex(RegexOption.MULTILINE)
-    private val WARNING_REGEX = """(LaTeX|Package [a-zA-Z_\-]+) Warning: (?<Message>[^\r\n]*)""".toRegex()
-    private val BAD_BOX_REGEX = """(?<Message>(Ov|Und)erfull \\[hv]box[^\r\n]*lines? (?<Line>\d+)[^\r\n]*)""".toRegex()
+    private const val TEX_ERROR_REGEX = """^! ((?<Message1>(.|\r|\n)*?)\r?\nl\.(?<Line>\d+)|(?<Message2>[^\r\n]*))"""
+    private const val WARNING_REGEX = """(LaTeX|Package [a-zA-Z_\-]+) Warning: (?<Message>[^\r\n]*)"""
+    private const val BAD_BOX_REGEX = """(?<Message>(Ov|Und)erfull \\[hv]box[^\r\n]*lines? (?<Line>\d+)[^\r\n]*)"""
 
     fun parse(parent: URI, log: String): List<BuildError> {
         val newLog = prepareLog(log)
@@ -20,26 +22,28 @@ object BuildErrorParser {
                 .sortedBy { it.length }
                 .toList()
 
-        fun parse(regex: Regex, kind: BuildErrorKind): Sequence<BuildError> = sequence {
-            for (match in regex.findAll(newLog)) {
-                val messageGroup = match.group("Message")
-                        ?: match.group("Message1")
-                        ?: match.group("Message2")
-                val message = messageGroup!!.value.lines()[0]
+        fun parse(regex: String, flags: Int, kind: BuildErrorKind): Sequence<BuildError> = sequence {
+            val pattern = Pattern.compile(regex, flags)
+            val matcher = pattern.matcher(newLog)
+            while (matcher.find()) {
+                val messageGroup = matcher.tryGroup("Message")
+                        ?: matcher.tryGroup("Message1")
+                        ?: matcher.tryGroup("Message2")
+                val message = messageGroup!!.lines()[0]
 
-                val file = ranges.first { it.contains(match.range.start) }.uri
+                val file = ranges.first { it.contains(matcher.start()) }.uri
                 val line = if (file == null) {
                     null
                 } else {
-                    match.group("Line")?.value?.toInt()?.let { it - 1 }
+                    matcher.tryGroup("Line")?.toInt()?.let { it - 1 }
                 }
                 yield(BuildError(file ?: parent, kind, message, line))
             }
         }
 
-        val texErrors = parse(TEX_ERROR_REGEX, BuildErrorKind.ERROR)
-        val warnings = parse(WARNING_REGEX, BuildErrorKind.WARNING)
-        val badBoxes = parse(BAD_BOX_REGEX, BuildErrorKind.WARNING)
+        val texErrors = parse(TEX_ERROR_REGEX, Pattern.MULTILINE, BuildErrorKind.ERROR)
+        val warnings = parse(WARNING_REGEX, 0, BuildErrorKind.WARNING)
+        val badBoxes = parse(BAD_BOX_REGEX, 0, BuildErrorKind.WARNING)
         return texErrors.plus(warnings)
                 .plus(badBoxes)
                 .toList()
@@ -54,7 +58,7 @@ object BuildErrorParser {
             val match = PACKAGE_MESSAGE_REGEX.matchEntire(line)
             when {
                 match != null -> {
-                    newLines[newLines.size - 1] += " " + match.groups["Message"]!!.value
+                    newLines[newLines.size - 1] += " " + match.groups[1]!!.value
                 }
                 line.endsWith("...") -> {
                     newLines.add(line.substring(0, line.length - 3))
@@ -86,7 +90,7 @@ object BuildErrorParser {
         }
 
         val basePath = Paths.get(File(parent).parent)
-        val fullPath = basePath.resolve(match.groups["File"]!!.value).normalize()
+        val fullPath = basePath.resolve(match.groups[1]!!.value).normalize()
         val uri = if (fullPath.startsWith(basePath)) {
             fullPath.toUri()
         } else {
@@ -96,9 +100,9 @@ object BuildErrorParser {
         return FileRange(uri, match.range.start, end)
     }
 
-    private fun MatchResult.group(name: String): MatchGroup? {
+    private fun Matcher.tryGroup(name: String): String? {
         return try {
-            groups[name]
+            return group(name)
         } catch (e: IllegalArgumentException) {
             null
         }
