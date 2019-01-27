1a93d224fbfd9fa866fd7e1f586ee98fcf6995e8
Patrick Förster <pfoerster@users.noreply.github.com>
2019-01-27 10:56:11 +0100

    Remove arguments parameter from main method

--- src/main/kotlin/texlab/Main.kt
+++ src/main/kotlin/texlab/Main.kt
@@ -2,7 +2,7 @@ package texlab
 
 import org.eclipse.lsp4j.jsonrpc.Launcher
 
-fun main(args: Array<String>) {
+fun main() {
     val server = LanguageServerImpl()
     val launcher = Launcher.createLauncher(server, CustomLanguageClient::class.java, System.`in`, System.out)
     server.connect(launcher.remoteProxy)
