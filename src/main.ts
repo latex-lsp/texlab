9ce8122133e543bbddd7413910e447a9d3e5524e
Eric Förster <efoerster@users.noreply.github.com>
2019-03-09 23:32:16 +0100

    Configure server functions

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,54 @@
-console.log('Hello World!');
+import {
+  createConnection,
+  ProposedFeatures,
+  TextDocuments,
+} from 'vscode-languageserver';
+
+const connection = createConnection(ProposedFeatures.all);
+const documents = new TextDocuments();
+
+connection.onInitialize(() => {
+  return {
+    capabilities: {
+      textDocumentSync: {
+        change: documents.syncKind,
+        save: { includeText: true },
+        openClose: true,
+      },
+      documentSymbolProvider: true,
+      renameProvider: true,
+      documentLinkProvider: { resolveProvider: false },
+      completionProvider: {
+        resolveProvider: true,
+        triggerCharacters: ['\\', '{', '}', '@', '/'],
+      },
+      foldingRangeProvider: true,
+      definitionProvider: true,
+      hoverProvider: true,
+      documentFormattingProvider: true,
+      referencesProvider: true,
+      documentHighlightProvider: true,
+    },
+  };
+});
+
+connection.onInitialized(() => {});
+connection.onDidChangeWatchedFiles(() => {});
+connection.onDidOpenTextDocument(() => {});
+connection.onDidChangeTextDocument(() => {});
+connection.onDidSaveTextDocument(() => {});
+connection.onDidCloseTextDocument(() => {});
+connection.onDocumentSymbol(() => null);
+connection.onRenameRequest(() => null);
+connection.onDocumentLinks(() => null);
+connection.onCompletion(() => null);
+connection.onCompletionResolve(x => x);
+connection.onFoldingRanges(() => null);
+connection.onDefinition(() => null);
+connection.onHover(() => null);
+connection.onDocumentFormatting(() => null);
+connection.onReferences(() => null);
+connection.onDocumentHighlight(() => null);
+
+documents.listen(connection);
+connection.listen();
