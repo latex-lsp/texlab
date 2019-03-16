58d2639774c6905d9f2d60adf5f1084b3dda96b4
Eric Förster <efoerster@users.noreply.github.com>
2019-03-16 19:15:01 +0100

    Refactor main.ts

---
+++ src/languageServer.ts
@@ -0,0 +1,319 @@
+import {
+  CodeAction,
+  CodeActionParams,
+  CodeLens,
+  CodeLensParams,
+  ColorInformation,
+  ColorPresentation,
+  ColorPresentationParams,
+  combineFeatures,
+  Command,
+  CompletionItem,
+  CompletionList,
+  CompletionParams,
+  Connection,
+  createConnection,
+  Declaration,
+  DeclarationLink,
+  Definition,
+  DefinitionLink,
+  DidChangeConfigurationParams,
+  DidChangeTextDocumentParams,
+  DidChangeWatchedFilesParams,
+  DidCloseTextDocumentParams,
+  DidOpenTextDocumentParams,
+  DidSaveTextDocumentParams,
+  DocumentColorParams,
+  DocumentFormattingParams,
+  DocumentHighlight,
+  DocumentLink,
+  DocumentLinkParams,
+  DocumentOnTypeFormattingParams,
+  DocumentRangeFormattingParams,
+  DocumentSymbol,
+  DocumentSymbolParams,
+  ExecuteCommandParams,
+  Features,
+  FoldingRange,
+  FoldingRangeParams,
+  Hover,
+  InitializedParams,
+  InitializeParams,
+  InitializeResult,
+  Location,
+  ProposedFeatures,
+  Range,
+  ReferenceParams,
+  RenameParams,
+  SignatureHelp,
+  SymbolInformation,
+  TextDocumentPositionParams,
+  TextEdit,
+  WillSaveTextDocumentParams,
+  WorkspaceEdit,
+  WorkspaceSymbolParams,
+} from 'vscode-languageserver';
+import { ProgressFeature, ProgressListener } from './protocol/progress';
+
+const CUSTOM_FEATURES: Features<{}, {}, {}, {}, ProgressListener> = {
+  __brand: 'features',
+  window: ProgressFeature,
+};
+
+export abstract class LanguageServer {
+  protected connection: Connection<{}, {}, {}, {}, ProgressListener, {}>;
+
+  constructor() {
+    const features = combineFeatures(ProposedFeatures.all, CUSTOM_FEATURES);
+    this.connection = createConnection(features);
+
+    this.connection.onInitialize(this.initialize.bind(this));
+    this.connection.onInitialized(this.initialized.bind(this));
+    this.connection.onShutdown(this.shutdown.bind(this));
+    this.connection.onInitialized(this.exit.bind(this));
+    this.connection.onDidChangeConfiguration(
+      this.didChangeConfiguration.bind(this),
+    );
+    this.connection.onDidChangeWatchedFiles(
+      this.didChangeWatchedFiles.bind(this),
+    );
+    this.connection.onDidOpenTextDocument(this.didOpenTextDocument.bind(this));
+    this.connection.onDidChangeTextDocument(
+      this.didChangeTextDocument.bind(this),
+    );
+    this.connection.onDidCloseTextDocument(
+      this.didCloseTextDocument.bind(this),
+    );
+    this.connection.onWillSaveTextDocument(
+      this.willSaveTextDocument.bind(this),
+    );
+    this.connection.onWillSaveTextDocumentWaitUntil(
+      this.willSaveTextDocumentWaitUntil.bind(this),
+    );
+    this.connection.onDidSaveTextDocument(this.didSaveTextDocument.bind(this));
+    this.connection.onHover(this.hover.bind(this));
+    this.connection.onCompletion(this.completion.bind(this));
+    this.connection.onCompletionResolve(this.completionResolve.bind(this));
+    this.connection.onSignatureHelp(this.signatureHelp.bind(this));
+    this.connection.onDeclaration(this.declaration.bind(this));
+    this.connection.onDefinition(this.definition.bind(this));
+    this.connection.onTypeDefinition(this.typeDefinition.bind(this));
+    this.connection.onImplementation(this.implementation.bind(this));
+    this.connection.onReferences(this.references.bind(this));
+    this.connection.onDocumentHighlight(this.documentHighlight.bind(this));
+    this.connection.onDocumentSymbol(this.documentSymbol.bind(this));
+    this.connection.onWorkspaceSymbol(this.workspaceSymbol.bind(this));
+    this.connection.onCodeAction(this.codeAction.bind(this));
+    this.connection.onCodeLens(this.codeLens.bind(this));
+    this.connection.onCodeLensResolve(this.codeLensResolve.bind(this));
+    this.connection.onDocumentFormatting(this.documentFormatting.bind(this));
+    this.connection.onDocumentRangeFormatting(
+      this.documentRangeFormatting.bind(this),
+    );
+    this.connection.onDocumentOnTypeFormatting(
+      this.documentOnTypeFormatting.bind(this),
+    );
+    this.connection.onRenameRequest(this.rename.bind(this));
+    this.connection.onPrepareRename(this.prepareRename.bind(this));
+    this.connection.onDocumentLinks(this.documentLinks.bind(this));
+    this.connection.onDocumentLinkResolve(this.documentLinkResolve.bind(this));
+    this.connection.onDocumentColor(this.documentColor.bind(this));
+    this.connection.onColorPresentation(this.colorPresentation.bind(this));
+    this.connection.onFoldingRanges(this.foldingRanges.bind(this));
+    this.connection.onExecuteCommand(this.executeCommand.bind(this));
+  }
+
+  public listen() {
+    this.connection.listen();
+  }
+
+  public abstract async initialize(
+    params: InitializeParams,
+  ): Promise<InitializeResult>;
+
+  public initialized(_params: InitializedParams): void {}
+
+  public async shutdown(): Promise<void> {}
+
+  public exit(): void {}
+
+  public didChangeConfiguration(_params: DidChangeConfigurationParams): void {}
+
+  public didChangeWatchedFiles(_params: DidChangeWatchedFilesParams): void {}
+
+  public didOpenTextDocument(_params: DidOpenTextDocumentParams): void {}
+
+  public didChangeTextDocument(_params: DidChangeTextDocumentParams): void {}
+
+  public didCloseTextDocument(_params: DidCloseTextDocumentParams): void {}
+
+  public willSaveTextDocument(_params: WillSaveTextDocumentParams): void {}
+
+  public async willSaveTextDocumentWaitUntil(
+    _params: WillSaveTextDocumentParams,
+  ): Promise<TextEdit[] | undefined | null> {
+    return undefined;
+  }
+
+  public didSaveTextDocument(_params: DidSaveTextDocumentParams): void {}
+
+  public async hover(
+    _params: TextDocumentPositionParams,
+  ): Promise<Hover | undefined | null> {
+    return undefined;
+  }
+
+  public async completion(
+    _params: CompletionParams,
+  ): Promise<CompletionItem[] | CompletionList | undefined | null> {
+    return undefined;
+  }
+
+  public async completionResolve(
+    item: CompletionItem,
+  ): Promise<CompletionItem> {
+    return item;
+  }
+
+  public async signatureHelp(
+    _params: TextDocumentPositionParams,
+  ): Promise<SignatureHelp | undefined | null> {
+    return undefined;
+  }
+
+  public async declaration(
+    _params: TextDocumentPositionParams,
+  ): Promise<Declaration | DeclarationLink[] | undefined | null> {
+    return undefined;
+  }
+
+  public async definition(
+    _params: TextDocumentPositionParams,
+  ): Promise<Definition | DefinitionLink[] | undefined | null> {
+    return undefined;
+  }
+
+  public async typeDefinition(
+    _params: TextDocumentPositionParams,
+  ): Promise<Definition | undefined | null> {
+    return undefined;
+  }
+
+  public async implementation(
+    _params: TextDocumentPositionParams,
+  ): Promise<Definition | undefined | null> {
+    return undefined;
+  }
+
+  public async references(
+    _params: ReferenceParams,
+  ): Promise<Location[] | undefined | null> {
+    return undefined;
+  }
+
+  public async documentHighlight(
+    _params: TextDocumentPositionParams,
+  ): Promise<DocumentHighlight[] | undefined | null> {
+    return undefined;
+  }
+
+  public async documentSymbol(
+    _params: DocumentSymbolParams,
+  ): Promise<SymbolInformation[] | DocumentSymbol[] | undefined | null> {
+    return undefined;
+  }
+
+  public async workspaceSymbol(
+    _params: WorkspaceSymbolParams,
+  ): Promise<SymbolInformation[] | undefined | null> {
+    return undefined;
+  }
+
+  public async codeAction(
+    _params: CodeActionParams,
+  ): Promise<Array<Command | CodeAction> | undefined | null> {
+    return undefined;
+  }
+
+  public async codeLens(
+    _params: CodeLensParams,
+  ): Promise<CodeLens[] | undefined | null> {
+    return undefined;
+  }
+
+  public async codeLensResolve(item: CodeLens): Promise<CodeLens> {
+    return item;
+  }
+
+  public async documentFormatting(
+    _params: DocumentFormattingParams,
+  ): Promise<TextEdit[] | undefined | null> {
+    return undefined;
+  }
+
+  public async documentRangeFormatting(
+    _params: DocumentRangeFormattingParams,
+  ): Promise<TextEdit[] | undefined | null> {
+    return undefined;
+  }
+
+  public async documentOnTypeFormatting(
+    _params: DocumentOnTypeFormattingParams,
+  ): Promise<TextEdit[] | undefined | null> {
+    return undefined;
+  }
+
+  public async rename(
+    _params: RenameParams,
+  ): Promise<WorkspaceEdit | undefined | null> {
+    return undefined;
+  }
+
+  public async prepareRename(
+    _params: TextDocumentPositionParams,
+  ): Promise<
+    | Range
+    | {
+        range: Range;
+        placeholder: string;
+      }
+    | undefined
+    | null
+  > {
+    return undefined;
+  }
+
+  public async documentLinks(
+    _params: DocumentLinkParams,
+  ): Promise<DocumentLink[] | undefined | null> {
+    return undefined;
+  }
+
+  public async documentLinkResolve(item: DocumentLink): Promise<DocumentLink> {
+    return item;
+  }
+
+  public async documentColor(
+    _params: DocumentColorParams,
+  ): Promise<ColorInformation[] | undefined | null> {
+    return undefined;
+  }
+
+  public async colorPresentation(
+    _params: ColorPresentationParams,
+  ): Promise<ColorPresentation[] | undefined | null> {
+    return undefined;
+  }
+
+  public async foldingRanges(
+    _params: FoldingRangeParams,
+  ): Promise<FoldingRange[] | undefined | null> {
+    return undefined;
+  }
+
+  public async executeCommand(
+    _params: ExecuteCommandParams,
+  ): Promise<any | undefined | null> {
+    return undefined;
+  }
+}
---
+++ src/latexLanguageServer.ts
@@ -0,0 +1,302 @@
+import {
+  CancellationToken,
+  CompletionItem,
+  CompletionItemKind,
+  CompletionList,
+  CompletionParams,
+  Definition,
+  DefinitionLink,
+  DidChangeTextDocumentParams,
+  DidOpenTextDocumentParams,
+  DocumentFormattingParams,
+  DocumentLink,
+  DocumentLinkParams,
+  FoldingRange,
+  FoldingRangeParams,
+  Hover,
+  InitializeParams,
+  InitializeResult,
+  ServerCapabilities,
+  TextDocumentIdentifier,
+  TextDocumentPositionParams,
+  TextDocumentSyncKind,
+  TextEdit,
+} from 'vscode-languageserver';
+import { BuildConfig, BuildProvider } from './build';
+import { CompletionProvider } from './completion';
+import { LatexComponentDatabase } from './completion/latex/data/component';
+import { COMPONENT_DATABASE_FILE } from './config';
+import { definitonProvider } from './definition';
+import { Document } from './document';
+import { foldingProvider } from './folding';
+import { BibtexFormatter, BibtexFormatterConfig } from './formatting/bibtex';
+import { ForwardSearchConfig, forwardSearchProvider } from './forwardSearch';
+import { hoverProvider } from './hover';
+import { getLanguageById, Language } from './language';
+import { LanguageServer } from './languageServer';
+import { linkProvider } from './link';
+import {
+  INVALID_DISTRIBUTION_MESSAGE,
+  KPSEWHICH_NOT_FOUND_MESSAGE,
+  UNKNOWN_DISTRIBUTION_MESSAGE,
+} from './messages';
+import {
+  BuildResult,
+  BuildTextDocumentParams,
+  BuildTextDocumentRequest,
+} from './protocol/build';
+import {
+  ForwardSearchRequest,
+  ForwardSearchResult,
+} from './protocol/forwardSearch';
+import { FeatureContext, FeatureProvider } from './provider';
+import {
+  createResolver,
+  TexDistributionError,
+  TexDistributionErrorKind,
+} from './resolver';
+import { BibtexSyntaxKind } from './syntax/bibtex/ast';
+import { Uri } from './uri';
+import { Workspace } from './workspace';
+
+export class LatexLanguageServer extends LanguageServer {
+  private readonly workspace = new Workspace();
+  private readonly resolver = createResolver();
+  private readonly componentDatabase = LatexComponentDatabase.create(
+    COMPONENT_DATABASE_FILE,
+    this.resolver,
+    this.connection.window,
+  );
+
+  private readonly completionProvider = CompletionProvider(
+    this.resolver,
+    this.componentDatabase,
+  );
+
+  private readonly buildProvider = BuildProvider(
+    this.connection.console,
+    this.connection.window,
+  );
+
+  constructor() {
+    super();
+
+    this.resolver.catch(error => {
+      if (error instanceof TexDistributionError) {
+        const { window } = this.connection;
+        switch (error.kind) {
+          case TexDistributionErrorKind.KpsewhichNotFound:
+            window.showErrorMessage(KPSEWHICH_NOT_FOUND_MESSAGE);
+            break;
+          case TexDistributionErrorKind.UnknownDistribution:
+            window.showErrorMessage(UNKNOWN_DISTRIBUTION_MESSAGE);
+            break;
+          case TexDistributionErrorKind.InvalidDistribution:
+            window.showErrorMessage(INVALID_DISTRIBUTION_MESSAGE);
+            break;
+        }
+      }
+    });
+
+    this.connection.onRequest(
+      BuildTextDocumentRequest.type,
+      this.build.bind(this),
+    );
+
+    this.connection.onRequest(
+      ForwardSearchRequest.type,
+      this.forwardSearch.bind(this),
+    );
+  }
+
+  public async initialize({
+    rootUri,
+  }: InitializeParams): Promise<InitializeResult> {
+    if (rootUri) {
+      const root = Uri.parse(rootUri);
+      if (root.isFile()) {
+        await this.workspace.loadDirectory(root);
+        await this.workspace.loadIncludes();
+      }
+    }
+
+    const capabilities: ServerCapabilities = {
+      textDocumentSync: {
+        change: TextDocumentSyncKind.Full,
+        save: { includeText: true },
+        openClose: true,
+      },
+      documentSymbolProvider: true,
+      renameProvider: true,
+      documentLinkProvider: { resolveProvider: false },
+      completionProvider: {
+        resolveProvider: true,
+        triggerCharacters: ['\\', '{', '}', '@', '/'],
+      },
+      foldingRangeProvider: true,
+      definitionProvider: true,
+      hoverProvider: true,
+      documentFormattingProvider: true,
+      referencesProvider: true,
+      documentHighlightProvider: true,
+    };
+
+    return { capabilities };
+  }
+
+  public didOpenTextDocument({
+    textDocument,
+  }: DidOpenTextDocumentParams): void {
+    const language = getLanguageById(textDocument.languageId);
+    if (language === undefined) {
+      return;
+    }
+
+    const uri = Uri.parse(textDocument.uri);
+    const document = Document.create(uri, textDocument.text, language);
+    this.workspace.put(document);
+
+    this.onDidOpenOrChange();
+  }
+
+  public didChangeTextDocument({
+    textDocument,
+    contentChanges,
+  }: DidChangeTextDocumentParams): void {
+    const uri = Uri.parse(textDocument.uri);
+    const text = contentChanges[0].text;
+    const { tree } = this.workspace.documents.find(x => x.uri.equals(uri))!;
+    const document = Document.create(uri, text, tree.language);
+    this.workspace.put(document);
+
+    this.onDidOpenOrChange();
+  }
+
+  public async hover(
+    params: TextDocumentPositionParams,
+  ): Promise<Hover | undefined | null> {
+    return this.runProvider(hoverProvider, params);
+  }
+
+  public async completion(
+    params: CompletionParams,
+  ): Promise<CompletionItem[] | CompletionList | undefined | null> {
+    const items = await this.runProvider(this.completionProvider, params);
+    const allIncludes = items.every(
+      x =>
+        x.kind === CompletionItemKind.Folder ||
+        x.kind === CompletionItemKind.File,
+    );
+
+    return {
+      isIncomplete: !allIncludes,
+      items,
+    };
+  }
+
+  public async definition(
+    params: TextDocumentPositionParams,
+  ): Promise<Definition | DefinitionLink[] | undefined | null> {
+    return this.runProvider(definitonProvider, params);
+  }
+
+  public async documentFormatting(
+    params: DocumentFormattingParams,
+  ): Promise<TextEdit[] | undefined | null> {
+    const uri = Uri.parse(params.textDocument.uri);
+    const document = this.workspace.documents.find(x => x.uri.equals(uri));
+    if (document === undefined || document.tree.language !== Language.Bibtex) {
+      return null;
+    }
+
+    const { insertSpaces, tabSize } = params.options;
+    const config: BibtexFormatterConfig = await this.connection.workspace.getConfiguration(
+      { section: 'bibtex.formatting' },
+    );
+
+    const formatter = new BibtexFormatter(
+      insertSpaces,
+      tabSize,
+      config.lineLength,
+    );
+
+    const edits: TextEdit[] = [];
+    document.tree.root.children.forEach(declaration => {
+      switch (declaration.kind) {
+        case BibtexSyntaxKind.Preamble:
+        case BibtexSyntaxKind.String:
+        case BibtexSyntaxKind.Entry:
+          edits.push({
+            range: declaration.range,
+            newText: formatter.format(declaration),
+          });
+          break;
+      }
+    });
+    return edits;
+  }
+
+  public async documentLinks(
+    params: DocumentLinkParams,
+  ): Promise<DocumentLink[] | undefined | null> {
+    return this.runProvider(linkProvider, params);
+  }
+
+  public async foldingRanges(
+    params: FoldingRangeParams,
+  ): Promise<FoldingRange[] | undefined | null> {
+    return this.runProvider(foldingProvider, params);
+  }
+
+  public async build(
+    params: BuildTextDocumentParams,
+    cancellationToken: CancellationToken,
+  ): Promise<BuildResult> {
+    const config: BuildConfig = await this.connection.workspace.getConfiguration(
+      {
+        section: 'latex.build',
+      },
+    );
+
+    return this.runProvider(
+      this.buildProvider,
+      { ...params, ...config },
+      cancellationToken,
+    );
+  }
+
+  public async forwardSearch(
+    params: TextDocumentPositionParams,
+  ): Promise<ForwardSearchResult> {
+    const config: ForwardSearchConfig = await this.connection.workspace.getConfiguration(
+      {
+        section: 'latex.forwardSearch',
+      },
+    );
+
+    return this.runProvider(forwardSearchProvider, {
+      ...params,
+      ...config,
+    });
+  }
+
+  private async onDidOpenOrChange() {
+    await this.workspace.loadIncludes();
+
+    this.workspace.documents
+      .map(x => this.workspace.relatedDocuments(x.uri))
+      .forEach(documents =>
+        this.componentDatabase.then(x => x.relatedComponents(documents)),
+      );
+  }
+
+  private async runProvider<T, R>(
+    provider: FeatureProvider<T, R>,
+    params: { textDocument: TextDocumentIdentifier } & T,
+    cancellationToken?: CancellationToken,
+  ): Promise<R> {
+    const uri = Uri.parse(params.textDocument.uri);
+    const context = new FeatureContext(uri, this.workspace, params);
+    return provider.execute(context, cancellationToken);
+  }
+}
