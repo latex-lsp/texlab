5402d2213a90c4e5afe2fe6aaa32aa3c2445bf5b
efoerster <efoerster@users.noreply.github.com>
2019-04-15 09:16:10 +0200

    Provide command line interface

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod range;
 mod server;
 mod syntax;
 
+use clap::*;
 use lsp::server::ServerBuilder;
 use server::LatexLspServer;
 use stderrlog::*;
@@ -10,8 +11,26 @@ use tokio;
 use tokio_stdin_stdout;
 
 fn main() {
+    let matches = app_from_crate!()
+        .author("")
+        .arg(
+            Arg::with_name("verbosity")
+                .short("v")
+                .multiple(true)
+                .help("Increase message verbosity"),
+        )
+        .arg(
+            Arg::with_name("quiet")
+                .long("quiet")
+                .short("q")
+                .help("No output printed to stderr"),
+        )
+        .get_matches();
+
     stderrlog::new()
         .module(module_path!())
+        .verbosity(matches.occurrences_of("verbosity") as usize)
+        .quiet(matches.is_present("quiet"))
         .timestamp(stderrlog::Timestamp::Off)
         .init()
         .unwrap();
