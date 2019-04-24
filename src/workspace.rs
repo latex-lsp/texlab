cba4c4e22247896e8d5f6a978f5fa7ec13732b6f
Patrick Förster <pfoerster@users.noreply.github.com>
2019-04-24 16:13:54 +0200

    Fix workspace on Windows

--- src/workspace.rs
+++ src/workspace.rs
@@ -163,7 +163,11 @@ fn resolve_link_targets(uri: &Url, relative_path: &str) -> Vec<String> {
     let mut path = PathBuf::from(uri.path());
     path.pop();
     path.push(relative_path);
-    let path = path.to_string_lossy().replace("\\", "/");
+    let mut path = path.to_string_lossy().replace("\\", "/");
+    if cfg!(windows) && path.starts_with("/") {
+        path.remove(0);
+    }
+
     for extension in DOCUMENT_EXTENSIONS {
         targets.push(format!("{}{}", path, extension));
     }
