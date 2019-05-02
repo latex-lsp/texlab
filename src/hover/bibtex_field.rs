4b5586604a8b6e979d9a9ff17457f55f020e0f9d
Patrick Förster <pfoerster@users.noreply.github.com>
2019-05-02 21:11:50 +0200

    Refactor hover tests

--- src/hover/bibtex_field.rs
+++ src/hover/bibtex_field.rs
@@ -1,8 +1,7 @@
 use crate::feature::FeatureRequest;
 use crate::metadata::bibtex_field;
-use crate::syntax::bibtex::ast::{BibtexField, BibtexVisitor};
+use crate::syntax::bibtex::ast::BibtexVisitor;
 use crate::syntax::bibtex::finder::{BibtexFinder, BibtexNode};
-use crate::syntax::text::SyntaxNode;
 use crate::workspace::SyntaxTree;
 use lsp_types::{Hover, HoverContents, MarkupContent, MarkupKind, TextDocumentPositionParams};
 
@@ -33,60 +32,79 @@ impl BibtexFieldHoverProvider {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::feature::FeatureTester;
-    use crate::workspace::WorkspaceBuilder;
-    use futures::executor::block_on;
+    use crate::completion::latex::data::types::LatexComponentDatabase;
+    use crate::feature::FeatureSpec;
+    use crate::test_feature;
+    use lsp_types::Position;
 
     #[test]
     fn test_known_field() {
-        let mut builder = WorkspaceBuilder::new();
-        let uri = builder.document("foo.bib", "@article{foo, author = bar}");
-        let request = FeatureTester::new(builder.workspace, uri, 0, 15, "").into();
-
-        let actual = block_on(BibtexFieldHoverProvider::execute(&request));
-
-        let expected = Hover {
-            contents: HoverContents::Markup(MarkupContent {
-                kind: MarkupKind::Markdown,
-                value: bibtex_field::get_documentation("author")
-                    .unwrap()
-                    .to_owned(),
-            }),
-            range: None,
-        };
-        assert_eq!(Some(expected), actual);
+        let hover = test_feature!(
+            BibtexFieldHoverProvider,
+            FeatureSpec {
+                files: vec![FeatureSpec::file("foo.bib", "@article{foo, author = bar}")],
+                main_file: "foo.bib",
+                position: Position::new(0, 15),
+                new_name: "",
+                component_database: LatexComponentDatabase::default(),
+            }
+        );
+        assert_eq!(
+            hover,
+            Some(Hover {
+                contents: HoverContents::Markup(MarkupContent {
+                    kind: MarkupKind::Markdown,
+                    value: bibtex_field::get_documentation("author")
+                        .unwrap()
+                        .to_owned(),
+                }),
+                range: None,
+            })
+        );
     }
 
     #[test]
     fn test_unknown_field() {
-        let mut builder = WorkspaceBuilder::new();
-        let uri = builder.document("foo.bib", "@article{foo, bar = baz}");
-        let request = FeatureTester::new(builder.workspace, uri, 0, 15, "").into();
-
-        let actual = block_on(BibtexFieldHoverProvider::execute(&request));
-
-        assert_eq!(None, actual);
+        let hover = test_feature!(
+            BibtexFieldHoverProvider,
+            FeatureSpec {
+                files: vec![FeatureSpec::file("foo.bib", "@article{foo, bar = baz}")],
+                main_file: "foo.bib",
+                position: Position::new(0, 15),
+                new_name: "",
+                component_database: LatexComponentDatabase::default(),
+            }
+        );
+        assert_eq!(hover, None);
     }
 
     #[test]
     fn test_entry_key() {
-        let mut builder = WorkspaceBuilder::new();
-        let uri = builder.document("foo.bib", "@article{foo, bar = baz}");
-        let request = FeatureTester::new(builder.workspace, uri, 0, 11, "").into();
-
-        let actual = block_on(BibtexFieldHoverProvider::execute(&request));
-
-        assert_eq!(None, actual);
+        let hover = test_feature!(
+            BibtexFieldHoverProvider,
+            FeatureSpec {
+                files: vec![FeatureSpec::file("foo.bib", "@article{foo, bar = baz}")],
+                main_file: "foo.bib",
+                position: Position::new(0, 11),
+                new_name: "",
+                component_database: LatexComponentDatabase::default(),
+            }
+        );
+        assert_eq!(hover, None);
     }
 
     #[test]
     fn test_latex() {
-        let mut builder = WorkspaceBuilder::new();
-        let uri = builder.document("foo.tex", "\\foo");
-        let request = FeatureTester::new(builder.workspace, uri, 0, 1, "").into();
-
-        let actual = block_on(BibtexFieldHoverProvider::execute(&request));
-
-        assert_eq!(None, actual);
+        let hover = test_feature!(
+            BibtexFieldHoverProvider,
+            FeatureSpec {
+                files: vec![FeatureSpec::file("foo.tex", "")],
+                main_file: "foo.tex",
+                position: Position::new(0, 0),
+                new_name: "",
+                component_database: LatexComponentDatabase::default(),
+            }
+        );
+        assert_eq!(hover, None);
     }
 }
