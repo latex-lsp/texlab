602dafdc444d9ab9841375c7718fdc88450082fd
Patrick Förster <pfoerster@users.noreply.github.com>
2019-07-16 10:15:41 +0200

    Download completion database before building

---
+++ build.rs
@@ -0,0 +1,9 @@
+use std::fs;
+
+fn main() {
+    let text = reqwest::get("https://github.com/latex-lsp/latex-completion-data/releases/download/v19.07/completion.json")
+        .expect("Failed to download completion database")
+        .text()
+        .unwrap();
+    fs::write("src/data/completion.json", text).expect("Failed to save completion database");
+}
