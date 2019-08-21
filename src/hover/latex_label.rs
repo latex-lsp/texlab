aedd00ac882c8a7f7bdf91ea484d61f95d04be33
Patrick Förster <pfoerster@users.noreply.github.com>
2019-08-21 21:10:11 +0200

    Fix label hovering

--- src/hover/latex_label.rs
+++ src/hover/latex_label.rs
@@ -1,7 +1,8 @@
 use crate::syntax::*;
 use crate::workspace::*;
 use futures_boxed::boxed;
-use lsp_types::{Hover, HoverContents, TextDocumentPositionParams};
+use lsp_types::*;
+use std::sync::Arc;
 
 pub struct LatexLabelHoverProvider;
 
@@ -12,26 +13,56 @@ impl FeatureProvider for LatexLabelHoverProvider {
     #[boxed]
     async fn execute<'a>(&'a self, request: &'a FeatureRequest<Self::Params>) -> Self::Output {
         if let SyntaxTree::Latex(tree) = &request.document().tree {
-            let reference = tree
-                .labels
-                .iter()
-                .flat_map(LatexLabel::names)
-                .find(|label| label.range().contains(request.params.position))?;
+            let reference = Self::find_reference(tree, request.params.position)?;
+            let (document, definition) = Self::find_definition(&request.view, reference)?;
 
-            let definition = tree
-                .labels
-                .iter()
-                .filter(|label| label.kind == LatexLabelKind::Definition)
-                .flat_map(LatexLabel::names)
-                .find(|label| label.text() == reference.text())?;
-
-            let outline = Outline::from(&request.view);
-            let context = OutlineContext::find(&outline, &request.view, definition.start());
+            let workspace = Arc::clone(&request.view.workspace);
+            let view = DocumentView::new(workspace, document);
+            let outline = Outline::from(&view);
+            let context = OutlineContext::find(&outline, &view, definition.start());
             let markup = context.formatted_reference()?;
             return Some(Hover {
                 contents: HoverContents::Markup(markup),
                 range: Some(reference.range()),
             });
+        }
+        None
+    }
+}
+
+impl LatexLabelHoverProvider {
+    fn find_reference(tree: &LatexSyntaxTree, position: Position) -> Option<&LatexToken> {
+        for label in &tree.labels {
+            let names = label.names();
+            if names.len() == 1 && label.range().contains(position) {
+                return Some(&label.names()[0]);
+            }
+
+            for name in &names {
+                if name.range().contains(position) {
+                    return Some(name);
+                }
+            }
+        }
+        None
+    }
+
+    fn find_definition<'a, 'b>(
+        view: &'a DocumentView,
+        reference: &'b LatexToken,
+    ) -> Option<(Arc<Document>, &'a LatexToken)> {
+        for document in &view.related_documents {
+            if let SyntaxTree::Latex(tree) = &document.tree {
+                for label in &tree.labels {
+                    if label.kind == LatexLabelKind::Definition {
+                        for name in label.names() {
+                            if name.text() == reference.text() {
+                                return Some((Arc::clone(&document), name));
+                            }
+                        }
+                    }
+                }
+            }
         }
         None
     }
