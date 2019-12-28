55ad518a160f4c8fe758b4c623aec306591e500a
Patrick Förster <pfoerster@users.noreply.github.com>
2019-12-28 10:47:43 +0100

    Treat DEF files as TeX files

--- crates/texlab_distro/src/language.rs
+++ crates/texlab_distro/src/language.rs
@@ -7,7 +7,7 @@ pub enum Language {
 impl Language {
     pub fn by_extension(extension: &str) -> Option<Self> {
         match extension.to_lowercase().as_ref() {
-            "tex" | "sty" | "cls" | "lco" | "aux" => Some(Language::Latex),
+            "tex" | "sty" | "cls" | "def" | "lco" | "aux" => Some(Language::Latex),
             "bib" | "bibtex" => Some(Language::Bibtex),
             _ => None,
         }
