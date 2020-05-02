5e1fb34e00cd4978635f4572c7fdac5b4f5989e6
Eric Förster <efoerster@users.noreply.github.com>
2020-05-02 18:22:24 +0200

    Add file logging option

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use clap::{app_from_crate, crate_authors, crate_description, crate_name, crate_version, Arg};
+use clap::{
+    app_from_crate, crate_authors, crate_description, crate_name, crate_version, Arg, ArgMatches,
+};
 use futures::{channel::mpsc, prelude::*};
 use jsonrpc::MessageHandler;
-use std::{env, error, sync::Arc};
-use stderrlog::{ColorChoice, Timestamp};
+use log::LevelFilter;
+use std::{env, error, fs::OpenOptions, sync::Arc};
+
 use texlab::server::LatexLspServer;
 use texlab_protocol::{LatexLspClient, LspCodec};
 use texlab_tex::DynamicDistribution;
@@ -10,13 +13,13 @@ use tokio_util::codec::{FramedRead, FramedWrite};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn error::Error>> {
-    let matches = app_from_crate!()
+    let opts = app_from_crate!()
         .author("")
         .arg(
             Arg::with_name("verbosity")
                 .short("v")
                 .multiple(true)
-                .help("Increase message verbosity"),
+                .help("Increase message verbosity (-vvvv for max verbosity)"),
         )
         .arg(
             Arg::with_name("quiet")
@@ -24,30 +27,15 @@ async fn main() -> Result<(), Box<dyn error::Error>> {
                 .short("q")
                 .help("No output printed to stderr"),
         )
+        .arg(
+            Arg::with_name("log_file")
+                .long("log-file")
+                .value_name("FILE")
+                .help("Send the logs to the given file"),
+        )
         .get_matches();
 
-    stderrlog::new()
-        .module(module_path!())
-        .module("jsonrpc")
-        .module("texlab-citeproc")
-        .module("texlab-completion")
-        .module("texlab-components")
-        .module("texlab-definition")
-        .module("texlab-feature")
-        .module("texlab-folding")
-        .module("texlab-hover")
-        .module("texlab-protocol")
-        .module("texlab-reference")
-        .module("texlab-rename")
-        .module("texlab-symbol")
-        .module("texlab-syntax")
-        .module("texlab-tex")
-        .verbosity(matches.occurrences_of("verbosity") as usize)
-        .quiet(matches.is_present("quiet"))
-        .timestamp(Timestamp::Off)
-        .color(ColorChoice::Never)
-        .init()
-        .expect("failed to initialize logger");
+    setup_logger(opts);
 
     let mut stdin = FramedRead::new(tokio::io::stdin(), LspCodec);
     let (stdout_tx, mut stdout_rx) = mpsc::channel(0);
@@ -77,4 +65,37 @@ async fn main() -> Result<(), Box<dyn error::Error>> {
     }
 
     Ok(())
+}
+
+fn setup_logger(opts: ArgMatches) {
+    let verbosity_level = if !opts.is_present("quiet") {
+        match opts.occurrences_of("verbosity") {
+            0 => LevelFilter::Error,
+            1 => LevelFilter::Warn,
+            2 => LevelFilter::Info,
+            3 => LevelFilter::Debug,
+            _ => LevelFilter::Trace,
+        }
+    } else {
+        LevelFilter::Off
+    };
+
+    let logger = fern::Dispatch::new()
+        .format(|out, message, record| out.finish(format_args!("{} - {}", record.level(), message)))
+        .level(verbosity_level)
+        .filter(|metadata| metadata.target() == "jsonrpc" || metadata.target().contains("texlab"))
+        .chain(std::io::stderr());
+
+    let logger = match opts.value_of("log_file") {
+        Some(log_file) => logger.chain(
+            OpenOptions::new()
+                .write(true)
+                .create(true)
+                .open(log_file)
+                .expect("failed to open log file"),
+        ),
+        None => logger,
+    };
+
+    logger.apply().expect("failed to initialize logger");
 }
