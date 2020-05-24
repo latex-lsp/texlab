b81059d56e185cd22a3baf3bcc3af16a7308e4c0
Patrick Förster <pfoerster@users.noreply.github.com>
2020-05-24 15:52:01 +0200

    Set isIncomplete dynamically

--- src/server.rs
+++ src/server.rs
@@ -3,7 +3,7 @@ use crate::citeproc::render_citation;
 
 use crate::{
     build::BuildProvider,
-    completion::{CompletionItemData, CompletionProvider},
+    completion::{CompletionItemData, CompletionProvider, COMPLETION_LIMIT},
     components::COMPONENT_DATABASE,
     config::ConfigManager,
     definition::DefinitionProvider,
@@ -253,9 +253,10 @@ impl<C: LspClient + Send + Sync + 'static> LatexLspServer<C> {
             req.params.text_document_position.position,
         );
 
+        let items = self.completion_provider.execute(&req).await;
         Ok(CompletionList {
-            is_incomplete: true,
-            items: self.completion_provider.execute(&req).await,
+            is_incomplete: items.len() >= COMPLETION_LIMIT,
+            items,
         })
     }
 
