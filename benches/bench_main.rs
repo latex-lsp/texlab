034e41151f3746e8b2a673d0931fca2d40abd925
Patrick Förster <pfoerster@users.noreply.github.com>
2022-06-12 14:10:10 +0200

    Switch to glassbench instead of criterion

--- benches/bench_main.rs
+++ benches/bench_main.rs
@@ -1,42 +1,43 @@
 use std::sync::Arc;
 
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use lsp_types::{
-    CompletionParams, Position, TextDocumentIdentifier, TextDocumentPositionParams, Url,
-};
+use glassbench::{glassbench, pretend_used, Bench};
+use lsp_types::{CompletionParams, Url};
 use texlab::{features::FeatureRequest, syntax::latex, DocumentLanguage, Workspace};
 
-fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("LaTeX/Parser", |b| {
-        b.iter(|| latex::parse(black_box(include_str!("../texlab.tex"))));
+fn bench_parser(bench: &mut Bench) {
+    bench.task("Parser/LaTeX", |task| {
+        task.iter(|| pretend_used(latex::parse(include_str!("../texlab.tex"))));
     });
+}
 
-    c.bench_function("LaTeX/Completion/Command", |b| {
+fn bench_completion(bench: &mut Bench) {
+    bench.task("Completion/LaTeX/Command", |task| {
         let uri = Arc::new(Url::parse("http://example.com/texlab.tex").unwrap());
         let text = Arc::new(include_str!("../texlab.tex").to_string());
-        let mut workspace = Workspace::default();
-        workspace
-            .open(Arc::clone(&uri), text, DocumentLanguage::Latex)
-            .unwrap();
+        let params: CompletionParams = serde_json::from_value(serde_json::json!({
+            "textDocument": {
+                "uri": uri.as_str()
+            },
+            "position": {
+                "line": 0u32,
+                "character": 1u32
+            }
+        }))
+        .unwrap();
 
-        b.iter(|| {
-            texlab::features::complete(FeatureRequest {
-                params: CompletionParams {
-                    context: None,
-                    partial_result_params: Default::default(),
-                    work_done_progress_params: Default::default(),
-                    text_document_position: TextDocumentPositionParams::new(
-                        TextDocumentIdentifier::new(uri.as_ref().clone()),
-                        Position::new(0, 1),
-                    ),
-                },
+        task.iter(|| {
+            let mut workspace = Workspace::default();
+            workspace
+                .open(Arc::clone(&uri), Arc::clone(&text), DocumentLanguage::Latex)
+                .unwrap();
+
+            pretend_used(texlab::features::complete(FeatureRequest {
+                params: params.clone(),
                 workspace: workspace.clone(),
                 uri: Arc::clone(&uri),
-            })
+            }))
         });
     });
 }
 
-criterion_group!(benches, criterion_benchmark);
-
-criterion_main!(benches);
+glassbench!("texlab", bench_parser, bench_completion,);
