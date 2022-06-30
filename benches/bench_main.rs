47f5f406d646d2c854cc09f01127cc5d4889eb4b
Patrick Förster <pfoerster@users.noreply.github.com>
2022-06-30 21:49:56 +0200

    Revert "Re-introduce brunch for benchmarking"
    This reverts commit 59c8c7fb9f0904341843ebe2ba8938bbd1b542e7.

--- benches/bench_main.rs
+++ benches/bench_main.rs
@@ -1,15 +1,17 @@
 use std::sync::Arc;
 
-use brunch::{benches, Bench};
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use lsp_types::{
     CompletionParams, Position, TextDocumentIdentifier, TextDocumentPositionParams, Url,
 };
 use texlab::{features::FeatureRequest, syntax::latex, DocumentLanguage, Workspace};
 
-benches!(
-    Bench::new("parser::latex", "texlab.tex")
-        .with(|| { latex::parse(include_str!("../texlab.tex")) }),
-    Bench::new("completion::latex", "command").with(|| {
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("LaTeX/Parser", |b| {
+        b.iter(|| latex::parse(black_box(include_str!("../texlab.tex"))));
+    });
+
+    c.bench_function("LaTeX/Completion/Command", |b| {
         let uri = Arc::new(Url::parse("http://example.com/texlab.tex").unwrap());
         let text = Arc::new(include_str!("../texlab.tex").to_string());
         let mut workspace = Workspace::default();
@@ -17,18 +19,24 @@ benches!(
             .open(Arc::clone(&uri), text, DocumentLanguage::Latex)
             .unwrap();
 
-        texlab::features::complete(FeatureRequest {
-            params: CompletionParams {
-                context: None,
-                partial_result_params: Default::default(),
-                work_done_progress_params: Default::default(),
-                text_document_position: TextDocumentPositionParams::new(
-                    TextDocumentIdentifier::new(uri.as_ref().clone()),
-                    Position::new(0, 1),
-                ),
-            },
-            workspace: workspace.clone(),
-            uri: Arc::clone(&uri),
-        })
-    }),
-);
+        b.iter(|| {
+            texlab::features::complete(FeatureRequest {
+                params: CompletionParams {
+                    context: None,
+                    partial_result_params: Default::default(),
+                    work_done_progress_params: Default::default(),
+                    text_document_position: TextDocumentPositionParams::new(
+                        TextDocumentIdentifier::new(uri.as_ref().clone()),
+                        Position::new(0, 1),
+                    ),
+                },
+                workspace: workspace.clone(),
+                uri: Arc::clone(&uri),
+            })
+        });
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+
+criterion_main!(benches);
