6806c026722e2547a0fa83a4caf65a2bfb0d3542
Patrick Förster <pfoerster@users.noreply.github.com>
2023-07-18 21:26:08 +0200

    Refactor tests of workspace commands crate

--- crates/commands/src/find_envs.rs
+++ crates/commands/src/find_envs.rs
@@ -9,11 +9,15 @@ pub struct EnvironmentMatch {
 }
 
 pub fn find_environments(document: &Document, offset: TextSize) -> Vec<EnvironmentMatch> {
-    let DocumentData::Tex(data) = &document.data else { return Vec::new() };
+    let DocumentData::Tex(data) = &document.data else {
+        return Vec::new();
+    };
 
     let root = latex::SyntaxNode::new_root(data.green.clone());
 
-    let Some(token) = root.token_at_offset(offset).right_biased() else { return Vec::new() };
+    let Some(token) = root.token_at_offset(offset).right_biased() else {
+        return Vec::new();
+    };
 
     let mut results = Vec::new();
     for environment in token
@@ -24,7 +28,10 @@ pub fn find_environments(document: &Document, offset: TextSize) -> Vec<Environme
             .begin()
             .and_then(|begin| begin.name())
             .and_then(|group| group.key())
-            .map(|name| Span::from(&name)) else { continue };
+            .map(|name| Span::from(&name))
+        else {
+            continue;
+        };
 
         let full_range = latex::small_range(&environment);
         results.push(EnvironmentMatch { name, full_range });
@@ -36,30 +43,82 @@ pub fn find_environments(document: &Document, offset: TextSize) -> Vec<Environme
 
 #[cfg(test)]
 mod tests {
-    use insta::assert_debug_snapshot;
+    use expect_test::{expect, Expect};
     use test_utils::fixture::Fixture;
 
     use crate::find_environments;
 
+    fn check(fixture: &str, expect: Expect) {
+        let fixture = Fixture::parse(fixture);
+        let workspace = fixture.workspace;
+        let document = workspace.iter().next().unwrap();
+        let offset = fixture.documents[0].cursor.unwrap();
+        let results = find_environments(&document, offset);
+        expect.assert_debug_eq(&results);
+    }
+
     #[test]
-    fn test() {
-        let fixture = Fixture::parse(
+    fn test_simple() {
+        check(
             r#"
 %! main.tex
 \begin{a}
-  \begin{b}
-    \begin{c}
-      |
-    \end{c}
-  \end{b}
-  \begin{d}
-  \end{d}
-\end{a}"#,
+    |
+\end{a}
+"#,
+            expect![[r#"
+                [
+                    EnvironmentMatch {
+                        name: Span(
+                            "a",
+                            7..8,
+                        ),
+                        full_range: 0..17,
+                    },
+                ]
+            "#]],
         );
+    }
 
-        let workspace = fixture.workspace;
-        let document = workspace.iter().next().unwrap();
-        let results = find_environments(&document, fixture.documents[0].cursor.unwrap());
-        assert_debug_snapshot!(results);
+    #[test]
+    fn test_nested() {
+        check(
+            r#"
+%! main.tex
+\begin{a}
+    \begin{b}
+        \begin{c}
+            |
+        \end{c}
+    \end{b}
+    \begin{d}
+    \end{d}
+\end{a}"#,
+            expect![[r#"
+                [
+                    EnvironmentMatch {
+                        name: Span(
+                            "a",
+                            7..8,
+                        ),
+                        full_range: 0..103,
+                    },
+                    EnvironmentMatch {
+                        name: Span(
+                            "b",
+                            21..22,
+                        ),
+                        full_range: 14..69,
+                    },
+                    EnvironmentMatch {
+                        name: Span(
+                            "c",
+                            39..40,
+                        ),
+                        full_range: 32..57,
+                    },
+                ]
+            "#]],
+        );
     }
 }
