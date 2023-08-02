3949b5afe63172e282223afaaf7cc1094558a2c0
Patrick Förster <pfoerster@users.noreply.github.com>
2023-08-02 20:59:24 +0200

    Show inlay hints for label references
    See #753.

--- crates/texlab/src/features/inlay_hint/label.rs
+++ crates/texlab/src/features/inlay_hint/label.rs
@@ -1,24 +1,30 @@
 use base_db::{
-    semantics::tex::LabelKind,
-    util::{render_label, RenderedObject},
+    semantics::tex::{Label, LabelKind},
+    util::{queries, render_label, RenderedLabel, RenderedObject},
     DocumentData,
 };
 
 use super::InlayHintBuilder;
 
 pub(super) fn find_hints(builder: &mut InlayHintBuilder) -> Option<()> {
-    let DocumentData::Tex(data) = &builder.document.data else { return None };
+    let DocumentData::Tex(data) = &builder.document.data else {
+        return None;
+    };
 
     let range = builder.range;
     for label in data
         .semantics
         .labels
         .iter()
-        .filter(|label| label.kind == LabelKind::Definition)
         .filter(|label| label.name.range.intersect(range).is_some())
     {
-        let Some(rendered) = render_label(builder.workspace, &builder.project, label) else { continue };
-        let Some(number) = &rendered.number else { continue };
+        let Some(rendered) = find_and_render(builder, &label.name.text) else {
+            continue;
+        };
+
+        let Some(number) = &rendered.number else {
+            continue;
+        };
 
         let text = match &rendered.object {
             RenderedObject::Section { prefix, .. } => {
@@ -38,4 +44,12 @@ pub(super) fn find_hints(builder: &mut InlayHintBuilder) -> Option<()> {
     }
 
     Some(())
+}
+
+fn find_and_render<'a>(builder: &InlayHintBuilder<'a>, name: &str) -> Option<RenderedLabel<'a>> {
+    let project = &builder.project;
+    queries::objects_with_name::<Label>(project, name)
+        .map(|(_, label)| label)
+        .find(|label| label.kind == LabelKind::Definition)
+        .and_then(|label| render_label(builder.workspace, project, label))
 }
