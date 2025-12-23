44c925abc9f1b637244b964e381be8a86641e4bc
Patrick Förster <pfoerster@users.noreply.github.com>
2025-12-23 22:05:52 +0100

    Use std::hint::black_box

--- crates/texlab/benches/bench_main.rs
+++ crates/texlab/benches/bench_main.rs
@@ -1,4 +1,6 @@
-use criterion::{Criterion, black_box, criterion_group, criterion_main};
+use std::hint::black_box;
+
+use criterion::{Criterion, criterion_group, criterion_main};
 use parser::{SyntaxConfig, parse_latex};
 
 const CODE: &str = include_str!("../../../texlab.tex");
